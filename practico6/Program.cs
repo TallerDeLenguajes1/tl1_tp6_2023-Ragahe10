2d0d276352d15a1ab8fcb72f7e16e46e1ab20049
RAMIRO <ragahe@gmail.com>
2023-05-18 18:48:07 -0300

    Ejercicio 01

---
+++ practico6/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// int a;
+// int b;
+// a= 10;
+// b=a;
+// Console.WriteLine("valor de a:"+a);
+// Console.WriteLine("valor de b:"+b);
+
+
+//EJERCICIO 1
+
+string entrada;
+int num, aux = 0, aux2; 
+bool resp;
+
+Console.Write("Ingrese un numero ");
+entrada = Console.ReadLine();
+
+resp = int.TryParse(entrada, out num);
+
+if(resp){
+    if(num > 0){
+        aux2 = num; 
+        if(num > 9){
+            while(num != 0){
+                aux = aux*10 + num%10;
+                num = num/10;
+            }
+        }else{
+            aux = aux2;
+        }
+        Console.WriteLine("El numero "+aux2+" con sus digitos invertidos es: "+ aux);
+    }else{
+        Console.WriteLine("El numero "+num+" no es mayor que cero");
+    }
+}else{
+    Console.WriteLine("No es numero");
+}
+//FIN EJERCICIO 1
